8c330500d76f12ad94b6d673c2dc0bc099e098c1
Howard Wang <howard122696@gmail.com>
2022-07-14 15:41:08 -0700

    exercise card

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -5,9 +5,9 @@ import {exerciseOptions, fetchData} from '../utils/fetchData'
 import HorizontalScrollbar from './HorizontalScrollbar'
 
 
-const SearchExercises = () => {
+const SearchExercises = ({setExercises, bodyPart, setBodyPart}) => {
   const [search, setSearch] = useState('')
-  const [exercises, setExercises] = useState([])
+
   const [bodyParts, setBodyParts] = useState([])
 
   useEffect(() => {
@@ -91,7 +91,7 @@ const SearchExercises = () => {
         p:'20px'
       }}
       >
-        <HorizontalScrollbar data={bodyParts}/>
+        <HorizontalScrollbar data={bodyParts} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
       </Box>
     </Stack>
   )
