5351b978307b669e8ec0b930ae094d00dd8fb585
Howard Wang <howard122696@gmail.com>
2022-07-15 17:19:37 -0700

    footer

--- src/components/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar.jsx
@@ -1,10 +1,11 @@
-import React, {useContext} from 'react'
-import { Box, Typography } from '@mui/material'
-import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu'
-import BodyPart from './BodyPart'
+import React, { useContext } from 'react';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import { Box, Typography } from '@mui/material';
 
-import RightArrowIcon from '../assets/icons/right-arrow.png'
-import LeftArrowIcon from '../assets/icons/left-arrow.png'
+import ExerciseCard from './ExerciseCard';
+import BodyPart from './BodyPart';
+import RightArrowIcon from '../assets/icons/right-arrow.png';
+import LeftArrowIcon from '../assets/icons/left-arrow.png';
 
 const LeftArrow = () => {
   const { scrollPrev } = useContext(VisibilityContext);
@@ -26,23 +27,19 @@ const RightArrow = () => {
   );
 };
 
+const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
+  <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+    {data.map((item) => (
+      <Box
+        key={item.id || item}
+        itemId={item.id || item}
+        title={item.id || item}
+        m="0 40px"
+      >
+        {bodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> : <ExerciseCard exercise={item} /> }
+      </Box>
+    ))}
+  </ScrollMenu>
+);
 
-const HorizontalScrollbar = ( {data, bodyParts, setBodyPart, bodyPart} ) => {
-  return (
-    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-      {data.map((item) => (
-        <Box
-          key = {item.id || item}
-          itemId = {item.id || item}
-          title= {item.id || item}
-          m='0 40px'
-        >
-          <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
-        </Box>
-        )
-      )}
-    </ScrollMenu>
-  )
-}
-
-export default HorizontalScrollbar
+export default HorizontalScrollbar;
